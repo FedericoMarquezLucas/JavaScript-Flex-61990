e18e30e8182ec1b4e03110156e8d97061772ba21
FedericoMarquezLucas <102321637+FedericoMarquezLucas@users.noreply.github.com>
2024-05-26 20:37:13 -0300

    Cart Alerts - Ajustes.

--- main.js
+++ main.js
@@ -1119,7 +1119,7 @@ CARTALERT.innerHTML += `
 		<div class="flex-shrink-0" id="alertIconContainer"></div>
 		<div class="flex flex-row gap-1.5 ml-3 text-sm font-medium">
 			<p><b id="alertDetalleMoto"></b></p>
-			<p>agregada al Carrito</p>
+			<p id="alertAccionMoto"></p>
 		</div>
 	</div>
 `
@@ -1636,10 +1636,12 @@ const alertaProductoCarrito = (marca, modelo, action) => {
 		document.querySelector('#cartAlert').classList.add(...addActionClasses)
 		document.querySelector('#cartAlert').classList.remove(...removeActionClasses)
 		document.querySelector('#alertIconContainer').innerHTML	= `<svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>`
+		document.querySelector('#alertAccionMoto').innerHTML = `agregada al Carrito`
 	} else if (action == 'remove') {
 		document.querySelector('#cartAlert').classList.add(...removeActionClasses)
 		document.querySelector('#cartAlert').classList.remove(...addActionClasses)
 		document.querySelector('#alertIconContainer').innerHTML	= `<svg class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>`
+		document.querySelector('#alertAccionMoto').innerHTML = `eliminada del Carrito`
 	}
 
 	const alertPosition = [ { bottom: '-50px' }, { bottom: '15px' } ]
