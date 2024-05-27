8f32760ca5fc5471e9482bbd7658f782a720aeae
FedericoMarquezLucas <102321637+FedericoMarquezLucas@users.noreply.github.com>
2024-05-27 15:03:54 -0300

    Ajustes Alerts.

--- main.js
+++ main.js
@@ -748,20 +748,9 @@ HEADER.innerHTML += `
 					</svg>
 				</button>
 			</div>
-
 			<div class="absolute right-0 z-10 mt-[27.5px] flex w-screen max-w-max" x-show="openWishlist" x-on:click.away="openWishlist = false">
 				<div class="w-screen max-w-md flex-auto overflow-hidden rounded-xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
 					<div id="listado-productos-wishlist" class="p-4"></div>
-					<div class="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-						<a href="#" class="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100">
-							<svg class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-								<path fill-rule="evenodd" d="M2 10a8 8 0 1116 0 8 8 0 01-16 0zm6.39-2.908a.75.75 0 01.766.027l3.5 2.25a.75.75 0 010 1.262l-3.5 2.25A.75.75 0 018 12.25v-4.5a.75.75 0 01.39-.658z" clip-rule="evenodd" />
-							</svg> Watch demo </a>
-						<a href="#" class="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100">
-							<svg class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-								<path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
-							</svg> Contact sales </a>
-					</div>
 				</div>
 			</div>
 		</div>
@@ -1664,14 +1653,14 @@ const alertaProductoCarrito = (marca, modelo, action) => {
 
 	if (action == 'add' || action == 'addWishlist' || action == 'movedToCart') {
 		document.querySelector('#cartAlert').classList.add(...addActionClasses)
-		document.querySelector('#cartAlert').classList.remove(...removeActionClasses)
+		document.querySelector('#cartAlert').classList.remove(...existsActionClasses,...removeActionClasses)
 		document.querySelector('#alertIconContainer').innerHTML	= `<svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>`
 		if (action == 'add') document.querySelector('#alertAccionMoto').innerHTML = `agregada al Carrito`
 		else if (action == 'addWishlist') document.querySelector('#alertAccionMoto').innerHTML = `agregada a Wishlist`
 		else document.querySelector('#alertAccionMoto').innerHTML = `movida al Carrito`
 	} else if (action == 'remove' || action == 'removeWishlist') {
 		document.querySelector('#cartAlert').classList.add(...removeActionClasses)
-		document.querySelector('#cartAlert').classList.remove(...addActionClasses)
+		document.querySelector('#cartAlert').classList.remove(...addActionClasses,...existsActionClasses)
 		document.querySelector('#alertIconContainer').innerHTML	= `<svg class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>`
 		if (action == 'remove') document.querySelector('#alertAccionMoto').innerHTML = `eliminada del Carrito`
 		else document.querySelector('#alertAccionMoto').innerHTML = `eliminada de Wishlist`
