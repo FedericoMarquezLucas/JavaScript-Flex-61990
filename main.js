674c8003079e9a2ab2d2cc0dec1996199652464a
FedericoMarquezLucas <102321637+FedericoMarquezLucas@users.noreply.github.com>
2024-06-03 13:13:12 -0300

    Product Rating Feature.
    > Se agrega Product Rating en Listado Producto, Modal Producto, Shopping Cart Slide Over.

--- main.js
+++ main.js
@@ -361,6 +361,7 @@ SECTIONHEADING.innerHTML += `
                             <option value="">- Defecto -</option>
                             <option value="precioAsc">Precio (ASC)</option>
                             <option value="precioDes">Precio (DES)</option>
+                            <option value="rating">Rating</option>
                             <option value="reciente">Recientes</option>
                         </select>
 
@@ -619,6 +620,17 @@ const irPagina = (page) => {
     mostrarPaginaPaginado(productos, paginaActual)
 }
 
+// PRODUCT RATING STARS.
+const ratingStars = rating => {
+    const starFilled = `<svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" /></svg>`
+    const starEmpty = `<svg class="size-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" /></svg>`
+
+    let ratingStarsIcons = ''
+    for (let i = 1; i <= 5; i++) { ratingStarsIcons += i <= rating ? starFilled : starEmpty }
+
+    return ratingStarsIcons
+}
+
 // LISTADO DE PRODUCTOS.
 const cargaGrillaDeProductos = (array, restoreStorage) => {
     // displayDeResultados(array)
@@ -628,6 +640,7 @@ const cargaGrillaDeProductos = (array, restoreStorage) => {
     for (const producto of array) {
         producto.kilometraje == '0' ? kilometrajeFormateado = '0km.' : kilometrajeFormateado = numeroFormateado(`${producto.precio}kms.`);
         producto.condicion == 'Nueva' ? nuevaBadge = '<span class="absolute top-3 right-3 items-center rounded-full bg-purple-100 px-2.5 py-1.5 text-xs font-semibold text-purple-700">NUEVA</span>' : nuevaBadge = '<span class="sr-only"></span>';
+        ratingStars(5)
 
         listadoProductos += `
             <div class="group">
@@ -639,9 +652,12 @@ const cargaGrillaDeProductos = (array, restoreStorage) => {
                     ${producto.marca}
                     <b>${producto.modelo}</b>
                 </h3>
-                <h5 class="mt-0 text-xs text-gray-500">
-                    ${producto.anio} | ${producto.cilindrada}${producto.cilindradaMedida} | ${kilometrajeFormateado}
-                </h5>
+                <div class="flex flex-row items-center gap-2.5">
+                    <h5 class="mt-0 text-xs text-gray-500">
+                        ${producto.anio} | ${producto.cilindrada}${producto.cilindradaMedida} | ${kilometrajeFormateado}
+                    </h5>
+                    <div class="flex flex-row items-center text-yellow-400">${ratingStars(producto.rating)}</div>
+                </div>
                 <div class="flex items-center justify-between mt-2.5">
                     <p class="text-lg font-semibold text-gray-900">
                         USD ${numeroFormateado(`${producto.precio}`)}
@@ -830,6 +846,7 @@ const sortListadoProductos = sort => {
 
     if (sort == 'precioAsc') arrayProductosOrdenados.sort((param1, param2) => param1.precio - param2.precio)
     else if (sort == 'precioDes') arrayProductosOrdenados.sort((param1, param2) => param2.precio - param1.precio)
+    else if (sort == 'rating') arrayProductosOrdenados.sort((param1, param2) => param2.rating - param1.rating)
     else if (sort == 'reciente') arrayProductosOrdenados.reverse()
 
     cargaGrillaDeProductos(arrayProductosOrdenados, true)
@@ -942,9 +959,13 @@ const triggerModal = productoId => {
 								<dt class="text-sm font-bold leading-6 text-gray-900">Kilometraje</dt>
 								<dd class="mt-1 text-sm leading-6 text-gray-700 col-span-2 sm:col-span-3 sm:mt-0">${kilometrajeFormateado}</dd>
 							</div>
-							<div class="py-2.5 pb-0 grid grid-cols-3 sm:grid-cols-4 gap-4">
+							<div class="py-2.5 grid grid-cols-3 sm:grid-cols-4 gap-4">
 								<dt class="text-sm font-bold leading-6 text-gray-900">Precio</dt>
 								<dd class="mt-1 text-sm leading-6 text-gray-700 col-span-2 sm:col-span-3 sm:mt-0">USD ${numeroFormateado(`${productoDetalleModal.precio}`)}</dd>
+							</div>
+							<div class="py-2.5 pb-0 grid grid-cols-3 sm:grid-cols-4 gap-4">
+								<dt class="text-sm font-bold leading-6 text-gray-900">Rating</dt>
+								<dd class="flex flex-row items-center mt-1 col-span-2 text-yellow-400 sm:col-span-3 sm:mt-0">${ratingStars(productoDetalleModal.rating)}</dd>
 							</div>
 						</dl>
                     </div>
@@ -1010,10 +1031,13 @@ const listadoProductosCart = array => {
 				<div class="pb-1.5 ml-3.5 flex flex-1 flex-col">
 					<div>
 						<div class="flex justify-between">
-							<h3 class="flex flex-col text-xs text-gray-700">
-								${producto.marca}
-								<b>${producto.modelo}</b>
-							</h3>
+                            <div class="flex flex-col gap-1.5">
+                                <h3 class="flex flex-col text-xs text-gray-700">
+                                    ${producto.marca}
+                                    <b>${producto.modelo}</b>
+                                </h3>
+                                <div class="flex flex-row items-center text-yellow-400">${ratingStars(producto.rating)}</div>
+                            </div>
 							<p class="text-sm text-gray-700 font-bold tracking-tighter">USD ${numeroFormateado(`${producto.precio}`)}</p>
 						</div>
 					</div>
